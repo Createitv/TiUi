2c3dbb470e886a70d63a1bc21f9a84e5f49170e1
Createitv <xfy150150@163.com>
2024-02-22 10:56:53 +0800

    Initial Commit

---
+++ Sources/components/button/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by DifferThink on 2/22/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
---
+++ Sources/tools/App.swift
@@ -0,0 +1,14 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by DifferThink on 2/22/24.
+//
+
+public struct TiUi {
+	public init() {}
+	
+	public func helloWorld() {
+		print("Hello, World!")
+	}
+}
